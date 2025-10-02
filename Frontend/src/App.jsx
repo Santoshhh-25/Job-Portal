4fc6978859f078c850880c2fc0886ff94d6b678a
Santoshh-25 <santoshdhamala.lp1190@gmail.com>
2025-10-02 23:08:18 +0530

    Fix import case sensitivity for Render deployment

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './components/Home'
 import Login from './components/auth/Login'
-import Signup from './components/auth/signup'
+import Signup from './components/auth/Signup'
 import Jobs from './components/Jobs'
 import Browse from './components/Browse'
 import Profile from './components/Profile'
