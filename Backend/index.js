269b5134cb8fefc1d64d469d0fd2d9e4e33bc100
Santoshh-25 <santoshdhamala.lp1190@gmail.com>
2025-10-02 23:28:29 +0530

    Fix production error

--- Backend/index.js
+++ Backend/index.js
@@ -21,11 +21,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true
 }
-
 app.use(cors(corsOptions));
+
 
 const PORT = process.env.PORT || 3000;
 
